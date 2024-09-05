4a3cdbcb8428814a5ed9c266d7b421c1f334a035
hhoulei <50575861+hhoulei@users.noreply.github.com>
2024-09-05 10:41:36 +0800

    Add files via upload

---
+++ multiple ancestries/code20240227_0902.R
@@ -0,0 +1,414 @@
+library(MASS)
+library(glmnet)
+library(doMC)
+library(doParallel)
+library(foreach)
+library(TwoSampleMR)
+
+g=100
+
+####data generation####
+DataGeneration <- function(g,b,rhob,siga1,siga2,race) {
+  
+  betaXG <- data.frame(
+    betaX1G = rnorm(g,0.2,0.05),
+    betaX2G = rnorm(g,0.2,0.05),
+    betaX3G = rnorm(g,0.2,0.05),
+    betaX4G = rnorm(g,0.2,0.03)
+  )
+  
+  
+  # betaXG <- data.frame(
+  #   betaX1G = c(rnorm(5,0.2,0.05),abs(rnorm(95,0,0.01))),
+  #   betaX2G = c(rnorm(5,0.2,0.05),abs(rnorm(95,0,0.01))),
+  #   betaX3G = c(rnorm(5,0.2,0.05),abs(rnorm(95,0,0.01))),
+  #   betaX4G = c(abs(rnorm(10,0,0.01)),rnorm(90,0.2,0.03))
+  # )
+  
+  seseby <- data.frame(
+    seseby1 <- runif(g,0.01,0.13),
+    seseby2 <- runif(g,0.02,0.12),
+    seseby3 <- runif(g,0.02,0.25),
+    seseby4 <- runif(g,0.0002,0.01)
+  )
+  
+  sigma_b <- data.frame(
+    sigma_b1 = seseby[,1]/betaXG[,1],
+    sigma_b2 = seseby[,2]/betaXG[,2],
+    sigma_b3 = seseby[,3]/betaXG[,3],
+    sigma_b4 = seseby[,4]/betaXG[,4]
+  )
+  
+  gamma <- data.frame(
+    gamma1 = runif(g,siga1,siga2),
+    gamma2 = runif(g,siga1,siga2),
+    gamma3 = runif(g,siga1,siga2),
+    gamma4 = runif(g,siga1,siga2))
+  
+  
+  betaY <- matrix(0, nrow = g, ncol = race)
+  sigma_matrices <- list() # 用于存储每个j循环生成的Sigma矩阵
+  
+  # 构建每对种族间的rho索引矩阵
+  # 输入的rhob里面应该有6个元素
+  rho_index <- 0
+  rho_matrix <- matrix(0, nrow = race, ncol = race)
+  for(r1 in 1:(race-1)){
+    for(r2 in (r1+1):race){
+      rho_index <- rho_index + 1
+      rho_matrix[r1, r2] <- rhob[rho_index]
+      rho_matrix[r2, r1] <- rhob[rho_index] # rho值是对称的
+    }
+  }
+  
+  for(j in 1:g){
+    Sigma <- matrix(0, nrow = race, ncol = race)
+    for(r1 in 1:race){
+      for(r2 in 1:race){
+        if(r1 == r2){
+          Sigma[r1, r2] <- (seseby[j,r1])^2
+        } else {
+          Sigma[r1, r2] <- rho_matrix[r1, r2] * seseby[j,r1] * seseby[j,r2]
+        }
+      }
+    }
+    
+    #cat('j=',j,'\n')
+    betaY_j <- mvrnorm(1, mu = as.numeric(b*betaXG[j,]+gamma[j,]), Sigma = Sigma)
+    betaY[j, ] <- betaY_j
+    
+    Sigma_mrtp <- matrix(0, nrow = race, ncol = race)
+    for(r1 in 1:race){
+      for(r2 in 1:race){
+        if(r1 == r2){
+          Sigma_mrtp[r1, r2] <- (sigma_b[j,r1])^2
+        } else {
+          Sigma_mrtp[r1, r2] <- rho_matrix[r1, r2] * sigma_b[j,r1] * sigma_b[j,r2]
+        }
+      }
+    }
+    
+    sigma_matrices[[j]] <- Sigma_mrtp # 存储Sigma矩阵
+  }
+  
+  beta <- (betaY/betaXG)
+  betat <- (betaY - gamma)/betaXG
+  
+  
+  # 动态构建data.frame
+  data_list <- list()
+  for(i in 1:ncol(beta)) {
+    data_list[[paste0("betat", i)]] <- betat[, i]
+    data_list[[paste0("sebetamrtp", i)]] <- sigma_b[,i]
+    data_list[[paste0("sebeta", i)]] <- seseby[,i]
+    data_list[[paste0("gamma", i)]] <- gamma[,i]
+    data_list[[paste0("betaXG", i)]] <- betaXG[, i]
+    data_list[[paste0("betaY", i)]] <- betaY[, i]
+  }
+  
+  data_sum <- as.data.frame(data_list)
+  
+  return(list(data_sum = data_sum, sigma_matrices = sigma_matrices))
+}
+
+
+####bootstrap####
+Bootstrap <- function(n_bootstrap,betat, betat_know, known_b, sigma_b_all, rhob, race, gb,initial_mle,b){
+  
+  # 初始化自举结果存储
+  bootstrap_results <- matrix(0, nrow = n_bootstrap, ncol = 2)  # 假设我们存储参数估计值和p值
+  
+  # 进行自举
+  for (i in 1:n_bootstrap) {
+    success <- FALSE
+    attempt <- 0
+    
+    while (!success && attempt < (maxRetries/2)) {
+      attempt <- attempt + 1
+      
+      # 生成重采样数据集
+      bootstrap_indices <- sample(length(sigma_b_all), size = 100, replace = TRUE)
+      
+      betat_n <- betat[bootstrap_indices]
+      betat_know_n <- betat_know[bootstrap_indices,]
+      sigma_b_all_n <- lapply(bootstrap_indices, function(idx) sigma_b_all[[idx]]) 
+      
+      # 使用MLikelihood函数计算参数估计值
+      res_mle <- MLikelihood(betat_n, betat_know_n, known_b, sigma_b_all_n, rhob, race, gb,b)
+      
+      if (!is.null(res_mle)) {
+        success <- TRUE
+        # 存储结果
+        bootstrap_results[i, ] <- res_mle}else {
+          bootstrap_results[i, ] <- c(NA,NA)}
+    }
+  }
+  
+  
+  # 计算置信区间
+  bootstrap_results <- na.omit(bootstrap_results)
+  confidence_interval <- quantile(bootstrap_results[,1], probs = c(0.025, 0.975))
+  if(initial_mle >= confidence_interval[1] && initial_mle <= confidence_interval[2]){
+    coverage_rate <- 1
+  }else{
+    coverage_rate <- 0
+  }
+  
+  return(coverage_rate)
+}
+
+coverCI <- function(beta,se,initial_b){
+  u_CI <- beta + se*1.96
+  l_CI <- beta - se*1.96
+  if(initial_b >= l_CI && initial_b <= u_CI){
+    coverage_rate <- 1
+  }else{
+    coverage_rate <- 0
+  }
+  
+  return(coverage_rate)
+}
+
+############### MRTP #############
+MLikelihood <- function(beta, beta_know,known_b, sigma_b_all, rhob, race, gb, b){
+  
+  gamma_know <- matrix(rep(0,300),ncol = 100)
+  gamma <- rep(0,100)
+  bbe <- b[2:4]
+  
+  # 定义 nll 函数
+  nll <- function(bbe1) {
+    
+    sigma_bb <- list()
+    pp <- NULL
+    det_sigma <- NULL
+    for (i in 1:gb) {
+      sigma_a0 <- as.numeric(sigma_b_all[[i]][1, 1])
+      sigma_a1 <- sigma_b_all[[i]][2:race, 1]
+      sigma_aa <- sigma_b_all[[i]][2:race, 2:race]
+      inv_sigma <- ginv(sigma_aa)
+      sigma_bb <- t(sigma_a1) %*% inv_sigma %*% sigma_a1
+      det_sigma[i] <- as.numeric(sigma_a0-sigma_bb)
+      
+      diff <- beta[i] - bbe1 - gamma[i] - sigma_a1 %*% inv_sigma %*% t(beta_know[i,] - known_b - gamma_know[,i])
+      pp[i] <- as.numeric(diff * diff / det_sigma[i] )
+    }
+    
+    sum(gb * log(2 * pi) + 0.5 * log(abs(det_sigma)) + 0.5 * pp)
+  }
+  
+  # 初始参数
+  #bba <- rep(0, (race - 1))
+  
+  tryFirstMethod <- function() {
+    
+    fit <- stats4::mle(minuslog=nll, 
+                       start=list(bbe1=0))
+    res <- fit@coef
+    
+    fit1 <- stats4::mle(minuslog=nll, 
+                        start=list(bbe1=0),
+                        fixed = list(bbe1=0))
+    
+    stat_beta1=  2 * (fit@min - fit1@min)
+    pvalue_beta1 = pchisq(-stat_beta1,1,lower.tail=F)
+    
+    return(c(res, pvalue_beta1))
+  }
+  
+  result <- tryCatch(
+    tryFirstMethod(),
+    error = function(e) NULL  # 如果第二种方法也失败，则跳过
+  )
+  
+  if (is.null(result)) {
+    cat("Both methods failed for the current iteration.\n")
+    return(NULL)  # 可能需要根据主函数的期望返回值进行调整
+  }
+  
+  return(result)
+}
+
+
+######COMP###########
+Comp <- function(x, gb,b,rhob,siga1,siga2,race, maxRetries) {
+  cat('x=', x, '\n')
+  
+  success <- FALSE
+  attempt <- 0
+  
+  while (!success && attempt < maxRetries) {
+    attempt <- attempt + 1
+    
+    fdata <- DataGeneration(gb,b,rhob,siga1,siga2,race)
+    
+    betat <- t(fdata$data_sum[grep("^betat\\d+$", colnames(fdata$data_sum), value = TRUE)[1]])
+    betat_know <- fdata$data_sum[grep("^betat\\d+$", colnames(fdata$data_sum), value = TRUE)[2:race]]
+    sigma_b_all <- fdata$sigma_matrices
+    known_b <- b[2:4]
+    
+    res_mle <- MLikelihood(betat, betat_know, known_b, sigma_b_all, rhob, race, gb,b)
+    coverage_TEMR <- Bootstrap(n_bootstrap,betat, betat_know, known_b,
+                               sigma_b_all, rhob, race, gb,b[1],b)
+    
+    if (!is.null(res_mle)) {
+      success <- TRUE
+      
+      ####Other MR
+      betaX1G <- t(fdata$data_sum[grep("^betaXG\\d+$", colnames(fdata$data_sum), value = TRUE)[-race]])
+      betaX1G <- betaX1G[1,]
+      betaY1G <- t(fdata$data_sum[grep("^betaY\\d+$", colnames(fdata$data_sum), value = TRUE)[-race]])
+      betaY1G <- betaY1G[1,]
+      sigma_b <- t(fdata$data_sum[grep("^sebeta\\d+$", colnames(fdata$data_sum), value = TRUE)[-race]])
+      sigma_b1 <- sigma_b[1,]
+      sigma_b2 <- sigma_b[2,]
+      
+      obj <- MendelianRandomization::mr_input(bx=betaX1G,bxse=sigma_b2,
+                                              by=betaY1G,byse = sigma_b1)
+      mr_ivw_re <- MendelianRandomization::mr_ivw(obj)
+      mr_simple_median_re <- mr_simple_median(betaX1G,betaY1G,sigma_b2,sigma_b1)
+      mr_weighted_median_re <- mr_weighted_median(betaX1G,betaY1G,sigma_b2,sigma_b1)
+      mr_penalised_weighted_median_re <- mr_penalised_weighted_median(betaX1G,betaY1G,sigma_b2,sigma_b1)
+      
+      coverage_ivw <- coverCI(mr_ivw_re$Estimate, mr_ivw_re@`StdError`,b[1])
+      coverage_simple_median <- coverCI(mr_simple_median_re$b, mr_simple_median_re$se,b[1])
+      coverage_weighted_median <- coverCI(mr_weighted_median_re$b, mr_weighted_median_re$se,b[1])
+      coverage_penalised_weighted_median <- coverCI(mr_penalised_weighted_median_re$b, 
+                                                    mr_penalised_weighted_median_re$se,b[1])
+      
+      
+      MR_dat <- data.frame(
+        beta.exposure=betaX1G,
+        beta.outcome=betaY1G, 
+        se.exposure=sigma_b2,
+        se.outcome=sigma_b1
+      )
+      mr_mode_re <- mr_mode(MR_dat, mode_method = "all")
+      simple_mode_coverage <- coverCI(mr_mode_re[1,5],mr_mode_re[1,6],b[1])
+      weighted_mode_coverage <- coverCI(mr_mode_re[2,5],mr_mode_re[2,6],b[1])
+      penalised_mode_coverage <- coverCI(mr_mode_re[3,5], mr_mode_re[3,6],b[1])
+      
+      res_all <- c(mr_ivw_re@Estimate, mr_ivw_re@StdError, mr_ivw_re@Pvalue, coverage_ivw,res_mle, coverage_TEMR,
+                   mr_simple_median_re$b, mr_weighted_median_re$b, mr_penalised_weighted_median_re$b, 
+                   mr_simple_median_re$se, mr_weighted_median_re$se, mr_penalised_weighted_median_re$se,
+                   mr_simple_median_re$pval, mr_weighted_median_re$pval, mr_penalised_weighted_median_re$pval,
+                   coverage_simple_median,coverage_simple_median,coverage_penalised_weighted_median,
+                   mr_mode_re[1,5], mr_mode_re[2,5], mr_mode_re[3,5],
+                   mr_mode_re[1,6], mr_mode_re[2,6], mr_mode_re[3,6],
+                   mr_mode_re[1,9], mr_mode_re[2,9], mr_mode_re[3,9],
+                   simple_mode_coverage,weighted_mode_coverage,penalised_mode_coverage,
+                   gb,b, rhob, race, siga1, siga2)
+      
+      res_all <- matrix(res_all ,nrow=1)
+      
+    } else {
+      cat("Both methods failed for the current iteration, retrying...\n")
+    }
+  }
+  
+  if (!success) {
+    return(NULL)  # Or handle the failure appropriately
+  }
+  
+  return(res_all)
+}
+
+
+#############MAIN############
+main <- function(NN,x,gb,b,rhob,siga1,siga2,race, maxRetries, mc){
+  
+  registerDoMC(mc)
+  
+  tt1 <- foreach(x=1:NN,
+                 .combine=rbind,
+                 .errorhandling = "remove",
+                 .packages = c("MASS","glmnet","TwoSampleMR")) %dopar% {
+                   Comp(x,gb,b,rhob,siga1,siga2,race, maxRetries)
+                 }
+  return(tt1)
+}
+
+
+##########SIMULATION##########
+# 示例参数
+# race = 4
+# Nb <- c(100, 100, 100, 100) # N1-N5
+# g=gb <- 10
+# pb <- c(0.1, 0.2, 0.3, 0.4) # p1-p5
+# betab <- c(1, 2, 3, 4) # beta1-beta5
+# sigma_xb <- c(1, 2, 3, 4) # sigma_x1-sigma_x5
+# bb <- rep(0,4)
+# rhob <- seq(0.1,0.6,0.1)
+# siga1=0
+# siga2=0.2
+
+#参数设置
+NN=1000
+maxRetries=10
+n_bootstrap=100
+mc=50
+race = 4
+gb <- 100
+
+bb_all <- list(rep(0,race),
+               rep(0.05,race)) # beta1-beta4相等
+rho_bb <- list(rep(0.1,sum(1:(race-1))),
+               rep(0.9,sum(1:(race-1))))#,
+               # c(0.9,0.9,-0.9,0.9,-0.9,-0.9),
+               # c(0.1,0.1,-0.1,0.1,-0.1,-0.1))
+
+siga1b <- c(0,0)
+siga2b <- c(0,0.01)
+
+######## 构建ccname向量##########
+# 定义beta和rho的个数
+n_beta <- race - 1
+n_rho <- sum(1:(race - 1))
+
+# 构建b和rho的字符串向量
+b_strings <- paste0("b", 1:race)
+rho_strings <- paste0("rho", 1:n_rho)
+
+ccname <- c('IVW_beta1','IVW_se1','IVW_pval1','IVW_coverage',
+            
+            'TEMR_beta1','TEMR_pvalue_beta1','TEMR_coverage',
+            
+            'simple_median_beta1','weighted_median_beta1','penalised_median_beta1',
+            'simple_median_se1','weighted_median_se1','penalised_median_se1',
+            'simple_median_pval1','weighted_median_pval1','penalised_median_pval1',
+            'simple_median_coverage','weighted_median_coverage','penalised_median_coverage',
+            
+            'simple_mode_beta1','weighted_mode_beta1','penalised_mode_beta1',
+            'simple_mode_se1','weighted_mode_se1','penalised_mode_se1',
+            'simple_mode_pval1','weighted_mode_pval1','penalised_mode_pval1',
+            'simple_mode_coverage','weighted_mode_coverage','penalised_mode_coverage',
+            'g',b_strings, rho_strings, 'race', "siga1", "siga2")
+
+# result <- NULL
+# for (x in 1:100) {
+#   res <- Comp(x,gb,b,rhob,siga1,siga2,race, maxRetries)
+#   result <- rbind(result,res)
+# }
+
+
+#########遍历##########
+m=n=t=p=q=1
+
+for(m in 1:length(bb_all)){
+  for(t in 1:length(rho_bb)){
+    for(p in 1:length(siga1b)){
+      b=bb_all[[m]]
+      rhob=rho_bb[[t]]
+      siga1=siga1b[p]
+      siga2=siga2b[p]
+      result <- main(NN,x, gb,b,rhob,siga1,siga2,race, maxRetries, mc)
+      #result <- Comp(x,gb,b,rhob,siga1,siga2,race, maxRetries)
+      result <- as.data.frame(result)
+      colnames(result) <- ccname
+      write.csv(result,paste0("MC","_g_",gb,"_bb_",b[1],"_rho_b_",rhob[6],
+                              "_siga1_",siga1,"_siga2_",siga2,"_race_",race,
+                              "_0902.csv"))
+      
+    }
+  }
+} 
---
+++ two ancestries/code202401200830.R
@@ -0,0 +1,301 @@
+# setwd("/home/houl/20240120/202401200825")
+# setwd("/home/jihanbing/MRTP-20240120")
+#setwd("C://Users//Lenovo//Desktop//异质人群MR//20231227")
+
+library(readr)
+library(MASS)
+library(doParallel)
+library(foreach)
+library(parallel)
+library(doMC)
+library(foreach)
+# library(AnchorRegression)
+library(TwoSampleMR)
+library(MendelianRandomization)
+
+##########data generation##########
+DataGeneration <- function(g,b1,b2,rho_b,siga1,siga2){
+  
+  
+  # betaX1G <- c(rnorm(5,0.2,0.05),abs(rnorm(95,0,0.01)))
+  # betaX2G <- c(abs(rnorm(10,0,0.01)),rnorm(90,0.2,0.03))
+  
+  betaX1G <- rnorm(g,0.2,0.05)
+  betaX2G <- rnorm(g,0.2,0.03)
+  
+  seseby1 <- runif(g,0.01,0.13)
+  seseby2 <- runif(g,0.0002,0.01)
+  
+  sigma_b1 <- seseby1/betaX1G
+  sigma_b2 <- seseby2/betaX2G
+  
+  gamma1 <- runif(g,siga1,siga2)
+  gamma2 <- runif(g,siga1,siga2)
+  
+  betaY1G <- NULL
+  betaY2G <- NULL
+  for(j in 1:g){
+    
+    betaY <- mvrnorm(1,mu = c(b1*betaX1G[j]+gamma1[j],
+                              b2*betaX2G[j]+gamma2[j]),
+                     Sigma = matrix(c(seseby1[j]^2,
+                                      seseby1[j]*seseby2[j]*rho_b,
+                                      seseby1[j]*seseby2[j]*rho_b,
+                                      seseby2[j]^2),
+                                    nrow=2))
+    betaY1G <- c(betaY1G,betaY[1])
+    betaY2G <- c(betaY2G,betaY[2])
+  }
+  
+  beta1 <- betaY1G/betaX1G
+  beta2 <- betaY2G/betaX2G
+  betat1 <- (betaY1G - gamma1)/betaX1G
+  betat2 <- (betaY2G - gamma2)/betaX2G
+  
+  
+  data_sum <- data.frame(betaY1G=betaY1G,
+                         betaY2G=betaY2G,
+                         betaX1G=betaX1G,
+                         betaX2G=betaX2G,
+                         sebeta1=seseby1,
+                         sebeta2=seseby2,
+                         sebeta1mrtp=sigma_b1,
+                         sebeta2mrtp=sigma_b2,
+                         gamma1=gamma1,
+                         gamma2=gamma2,
+                         betat1=betat1,
+                         betat2=betat2,
+                         rho_b=rho_b)
+  
+  return(data_sum)
+}
+
+####bootstrap####
+Bootstrap <- function(n_bootstrap,dataset,initial_mle){
+  
+  # 初始化自举结果存储
+  bootstrap_results <- matrix(0, nrow = n_bootstrap, ncol = 2)  # 假设我们存储参数估计值和p值
+  
+  # 进行自举
+  for (i in 1:n_bootstrap) {
+    # 生成重采样数据集
+    dataset_boot <- dataset[sample(nrow(dataset), size = 100, replace = TRUE), ]
+    
+    # 使用MLikelihood函数计算参数估计值
+    res_mle <- MLikelihood(dataset_boot$betat1, dataset_boot$betat2, b2, dataset_boot$sebeta1, dataset_boot$sebeta2, rho_b, dataset_boot$gamma1, dataset_boot$gamma2)
+    
+    # 存储结果
+    bootstrap_results[i, ] <- res_mle
+  }
+  
+  # 计算置信区间
+  confidence_interval <- quantile(bootstrap_results[,1], probs = c(0.025, 0.975))
+  if(initial_mle >= confidence_interval[1] && initial_mle <= confidence_interval[2]){
+    coverage_rate <- 1
+  }else{
+    coverage_rate <- 0
+  }
+  
+  return(coverage_rate)
+}
+
+coverCI <- function(beta,se,initial_b){
+  u_CI <- beta + se*1.96
+  l_CI <- beta - se*1.96
+  if(initial_b >= l_CI && initial_b <= u_CI){
+    coverage_rate <- 1
+  }else{
+    coverage_rate <- 0
+  }
+  
+  return(coverage_rate)
+}
+
+
+
+############### MRTP #############
+MLikelihood <- function(betat1,betat2,b2,sigma_b1,sigma_b2,rho_b,gamma1,gamma2){
+  
+  bb2=b2
+  ggamma1=0
+  ggamma2=0
+  
+  nll <- function(bb1){
+    det_sigma = (1-rho_b^2)*(sigma_b1^2)
+    pp = (betat1-bb1-ggamma1-rho_b*sigma_b1*(betat2-bb2-ggamma2)/sigma_b2)^2
+    sum(log(2*pi)+0.5*log(det_sigma)+0.5*pp/det_sigma)
+  }
+  
+  fit <- stats4::mle(minuslog=nll, 
+                     start=list(bb1=0))
+  res <- fit@coef
+  
+  fit1 <- stats4::mle(minuslog=nll, 
+                      start=list(bb1=0),
+                      fixed = list(bb1=0) )
+  
+  stat_beta1=  2 * (fit@min - fit1@min)
+  pvalue_beta1 = pchisq(-stat_beta1,1,lower.tail=F)
+  
+  return(c(res,pvalue_beta1))
+}
+
+
+######COMP###########
+Comp <- function(x,g,b1,b2,rho_b,siga1,siga2){
+  cat('x=',x,'\n')
+  dataset <- DataGeneration(g,b1,b2,rho_b,siga1,siga2)
+  
+  #####MRTP
+  sigma_b1 <- dataset$sebeta1mrtp
+  sigma_b2 <- dataset$sebeta2mrtp
+  gamma1 <- dataset$gamma1
+  gamma2 <- dataset$gamma2
+  
+  betat1 <- dataset$betat1
+  betat2 <- dataset$betat2
+  
+  res_mle <- MLikelihood(betat1,betat2,b2,sigma_b1,sigma_b2,rho_b,gamma1,gamma2)
+  res_mle
+  
+  coverage_TEMR <- Bootstrap(n_bootstrap,dataset,b1)
+  
+  ####Other MR
+  obj <- MendelianRandomization::mr_input(bx=dataset$betaX1G,bxse=dataset$sebeta2,
+                                          by=dataset$betaY1G,byse = dataset$sebeta1)
+  mr_ivw_re <- MendelianRandomization::mr_ivw(obj)
+  # mr_ivw_re <- mr_ivw(dataset$betaX1G,dataset$betaY1G,dataset$sebeta2,dataset$sebeta1)
+  coverage_ivw <- coverCI(mr_ivw_re$Estimate, mr_ivw_re@`StdError`,b1)
+  mr_simple_median_re <- mr_simple_median(dataset$betaX1G,dataset$betaY1G,dataset$sebeta2,dataset$sebeta1)
+  coverage_simple_median <- coverCI(mr_simple_median_re$b, mr_simple_median_re$se,b1)
+  mr_weighted_median_re <- mr_weighted_median(dataset$betaX1G,dataset$betaY1G,dataset$sebeta2,dataset$sebeta1)
+  coverage_weighted_median <- coverCI(mr_weighted_median_re$b, mr_weighted_median_re$se,b1)
+  mr_penalised_weighted_median_re <- mr_penalised_weighted_median(dataset$betaX1G,dataset$betaY1G,dataset$sebeta2,dataset$sebeta1)
+  coverage_penalised_weighted_median <- coverCI(mr_penalised_weighted_median_re$b, mr_penalised_weighted_median_re$se,b1)
+  
+  MR_dat <- data.frame(
+    beta.exposure=dataset$betaX1G,
+    beta.outcome=dataset$betaY1G, 
+    se.exposure=dataset$sebeta2,
+    se.outcome=dataset$sebeta1
+  )
+  mr_mode_re <- mr_mode(MR_dat, mode_method = "all")
+  simple_mode_coverage <- coverCI(mr_mode_re[1,5],mr_mode_re[1,6],b1)
+  weighted_mode_coverage <- coverCI(mr_mode_re[2,5],mr_mode_re[2,6],b1)
+  penalised_mode_coverage <- coverCI(mr_mode_re[3,5], mr_mode_re[3,6],b1)
+  
+  
+  res_all <- c(mr_ivw_re@Estimate, mr_ivw_re@StdError, mr_ivw_re@Pvalue, coverage_ivw, res_mle, coverage_TEMR,
+               mr_simple_median_re$b, mr_weighted_median_re$b, mr_penalised_weighted_median_re$b, 
+               mr_simple_median_re$se, mr_weighted_median_re$se, mr_penalised_weighted_median_re$se,
+               mr_simple_median_re$pval, mr_weighted_median_re$pval, mr_penalised_weighted_median_re$pval,
+               coverage_simple_median,coverage_simple_median,coverage_penalised_weighted_median,
+               mr_mode_re[1,5], mr_mode_re[2,5], mr_mode_re[3,5],
+               mr_mode_re[1,6], mr_mode_re[2,6], mr_mode_re[3,6],
+               mr_mode_re[1,9], mr_mode_re[2,9], mr_mode_re[3,9],
+               simple_mode_coverage,weighted_mode_coverage,penalised_mode_coverage,
+               g,b1,b2,rho_b,siga1,siga2)
+  
+  res_all <- matrix(res_all ,nrow=1)
+  
+  return(res_all)
+}
+
+#############MAIN############
+main <- function(NN,x,g,b1,b2,rho_b,siga1,siga2,mc){
+  
+  registerDoMC(mc)
+  
+  tt1 <- foreach(x=1:NN,
+                 .combine=rbind,
+                 .errorhandling = "remove",
+                 .packages = c("MASS")) %dopar% {
+                   Comp(x,g,b1,b2,rho_b,siga1,siga2)
+                 }
+  
+  # cl <- makeCluster(mc)
+  # registerDoParallel(cl)
+  # 
+  # tt1 <- foreach(x=1:NN,
+  #                .combine=rbind,
+  #                .packages=c("MASS","AnchorRegression")) %dopar% {
+  #                  Comp(x,betaX1G,betaX2G,g,b1,b2,rho_b,siga1,siga2)
+  #                }
+  # stopCluster(cl)
+  
+  
+  return(tt1)
+}
+
+
+##########SIMULATION##########
+ccname <- c('IVW_beta1','IVW_se1','IVW_pval1','IVW_coverage',
+            
+            'TEMR_beta1','TEMR_pvalue_beta1','TEMR_coverage',
+            
+            'simple_median_beta1','weighted_median_beta1','penalised_median_beta1',
+            'simple_median_se1','weighted_median_se1','penalised_median_se1',
+            'simple_median_pval1','weighted_median_pval1','penalised_median_pval1',
+            'simple_median_coverage','weighted_median_coverage','penalised_median_coverage',
+            
+            'simple_mode_beta1','weighted_mode_beta1','penalised_mode_beta1',
+            'simple_mode_se1','weighted_mode_se1','penalised_mode_se1',
+            'simple_mode_pval1','weighted_mode_pval1','penalised_mode_pval1',
+            'simple_mode_coverage','weighted_mode_coverage','penalised_mode_coverage',
+            
+            'g','b1','b2','rho_b',"siga1","siga2")
+
+# result1 <- NULL
+# for (x in 1:1000) {
+# 
+#   result <- Comp(x,betaX1G,betaX2G,g,b1,b2,rho_b,siga1,siga2)
+#   result1 <- rbind(result1,result)
+# }
+# result2 <- data.frame(result1)
+# colnames(result2) <- ccname
+
+NN=1000
+mc=20
+N1b=N3b=3000
+N2b=300000
+N4b=300000
+
+n_bootstrap <- 1000
+bb1 <- c(0,0.05)
+bb2 <- c(0,0.05)
+# bb1 <- c(0,0.05,0.1,0.15,0.2)
+# bb2 <- c(0,0.05,0.1,0.15,0.2)
+rho_bb <- c(0.1,0.2,0.4,0.6,0.8,0.9)
+siga1b <- c(0,0)
+siga2b <- c(0,0.01)
+g=100
+
+
+#m=n=t=p=z=p=1
+result_mean <- NULL
+for(p in 1:length(siga1b)){
+  for(n in 1:length(bb1)){
+    for(m in 1:length(bb2)){
+      for(t in 1:length(rho_bb)){
+        b1=bb1[n]
+        b2=bb2[m]
+        rho_b=rho_bb[t]
+        siga1=siga1b[p]
+        siga2=siga2b[p]
+        result <- main(NN,x,g,b1,b2,rho_b,siga1,siga2,mc)
+        #result <- Comp(x,betaX1G,betaX2G,g,b1,b2,rho_b,siga1,siga2)
+        # result <- NULL
+        # for (x in 1:1000) {
+        #   
+        #   result1 <- Comp(x,betaX1G,betaX2G,g,b1,b2,rho_b,siga1,siga2)
+        #   result <- rbind(result1,result)
+        # }
+        result <- as.data.frame(result)
+        colnames(result) <- ccname
+        write.csv(result,paste0("MC","_N2_",N2b,"_N4_",N4b,"_g_",g,
+                                "_beta1_",b1,"_beta2_",b2,"_rho_b_",rho_b,
+                                "_siga1_",siga1,"_siga2_",siga2,"_0830.csv"))
+      } 
+    }
+  }
+}
